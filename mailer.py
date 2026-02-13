a14709f00809445067a4426293825e681e9c48ce
Nicocarello <43417894+Nicocarello@users.noreply.github.com>
2026-02-13 01:56:41 -0300

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -257,8 +257,8 @@ def format_email_html(df, window_label):
 
 def send_email(subject, body):
     """Envía el correo usando SMTP"""
-    #recipients = [r.strip() for r in RECIPIENTS if r.strip()]
-    recipients = ["nicolas.carello@publicalatam.com"]
+    recipients = [r.strip() for r in RECIPIENTS if r.strip()]
+    #recipients = ["nicolas.carello@publicalatam.com"]
     if not recipients:
         print("⚠️ No hay destinatarios en EMAIL_TO.")
         return
