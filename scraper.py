eb12527c65d21bc84ea7f78d0901dcd452fb029c
Nicocarello <43417894+Nicocarello@users.noreply.github.com>
2026-03-11 15:46:15 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -349,8 +349,8 @@ final_df['article_body'] = final_df['link'].map(lambda u: link_to_body.get(url_k
 # Filtro robusto (keep only rows mentioning TikTok)
 # ---------------------------
 mask = (
-    final_df.get('title', '').astype(str).str.contains(TIKTOK_PATTERN, na=False) |
-    final_df.get('snippet', '').astype(str).str.contains(TIKTOK_PATTERN, na=False) |
+    # final_df.get('title', '').astype(str).str.contains(TIKTOK_PATTERN, na=False) |
+    # final_df.get('snippet', '').astype(str).str.contains(TIKTOK_PATTERN, na=False) |
     final_df.get('article_body', '').astype(str).str.contains(TIKTOK_PATTERN, na=False)
 )
 before_tot = len(final_df)
