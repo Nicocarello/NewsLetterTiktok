871d746e2d6025be31ca8b9f9c9dec10f36e65f1
Nicocarello <43417894+Nicocarello@users.noreply.github.com>
2026-03-17 14:07:51 -0300

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -260,8 +260,8 @@ def format_email_html(df, window_label, competencia_df=None):
 
 
 def send_email(subject, body):
-    recipients = [r.strip() for r in RECIPIENTS if r.strip()]
-    #recipients = ["nicolas.carello@publicalatam.com"]
+    #recipients = [r.strip() for r in RECIPIENTS if r.strip()]
+    recipients = ["nicolas.carello@publicalatam.com","victoria.arrudi@publicalatam.com"]
     if not recipients:
         print("⚠️ No hay destinatarios en EMAIL_TO.")
         return
