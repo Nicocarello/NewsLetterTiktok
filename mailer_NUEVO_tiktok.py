6b601d408563a558d313492fbfc3e551767f0b13
suscripciones-publica <suscripciones@publicalatam.com>
2026-04-29 14:44:17 -0300

    Update mailer_NUEVO_tiktok.py

--- mailer_NUEVO_tiktok.py
+++ mailer_NUEVO_tiktok.py
@@ -227,7 +227,7 @@ def format_email_html(df, window_label):
 
 # === EMAIL ===
 def send_email(subject, body):
-    recipients = ["victoria.arrudi@publicalatam.com, luz@publicalatam.com, sofia.szekasy@publicalatam.com"]
+    recipients = ["victoria.arrudi@publicalatam.com", "luz@publicalatam.com", "sofia.szekasy@publicalatam.com"]
 
     msg = MIMEText(body, "html", "utf-8")
     msg["Subject"] = subject
