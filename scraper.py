7728a3c78f15173e7c6b8d239c69d61d61c82b97
suscripciones-publica <suscripciones@publicalatam.com>
2026-05-11 16:19:09 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -55,7 +55,7 @@ if not APIFY_TOKEN:
 COUNTRIES = [c.strip() for c in os.getenv("COUNTRIES", "ar,cl,pe").split(",") if c.strip()]
 QUERIES = [q.strip() for q in os.getenv(
     "QUERIES",
-    "tik-tok,tiktok,tiktok grooming,tiktok estafa,booktok"
+    "tik-tok,tik tok,tiktok,tiktok grooming,tiktok estafa,booktok"
 ).split(",") if q.strip()]
 
 try:
