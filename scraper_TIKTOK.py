df70f0a011ea300bda7884d3fc07055efe8d4378
suscripciones-publica <suscripciones@publicalatam.com>
2026-08-25 11:19:08 -0300

    Update scraper_TIKTOK.py

--- scraper_TIKTOK.py
+++ scraper_TIKTOK.py
@@ -370,6 +370,15 @@ if 'link' in final_df.columns:
 
     final_df['link'] = final_df['link'].apply(normalize_link)
     final_df.drop_duplicates(subset=["link"], inplace=True)
+
+    # Excluir links no resueltos tipo /goto?url=...
+    final_df = final_df[
+        ~final_df['link'].astype(str).str.contains(
+            r'/goto',
+            case=False,
+            na=False
+        )
+    ].copy()
 else:
     logging.warning("No 'link' column present in scraped items; duplicates won't be removed by link.")
 
